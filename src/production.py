ce4d668bdc2e3d975cd1b86d0062e67bfeb48b80
Emilia Sarna <emilka.sarna@gmail.com>
2025-12-18 15:15:35 +0100

    added changing positions after production

--- src/production.py
+++ src/production.py
@@ -1,6 +1,7 @@
+from Cryptodome.SelfTest.Cipher.test_SIV import transform
 from matplotlib import pyplot as plt
+import numpy as np
 from .graph import Graph
-import networkx as nx
 
 
 class Production:
@@ -32,7 +33,7 @@ class Production:
         return K_graph
 
     # zastosowanie produkcji
-    def apply(self, input: Graph, mapping: list[int]) -> Graph:
+    def apply(self, input: Graph, mapping: list[int], transform_positions: bool = False) -> Graph:
 
         # kopia wejścia
         G = input
@@ -150,6 +151,9 @@ class Production:
                 continue  # jeśli krawędź już istnieje
             output.add_edge(u_out, v_out)
 
+        if transform_positions:
+            self.update_positions(input, output, mapping)
+
         return output
 
     def draw(self, title: str | None = None):
@@ -187,3 +191,95 @@ class Production:
 
         plt.tight_layout()
         plt.show()
+
+
+    def update_positions(self, input_graph: Graph, output_graph: Graph, mapping: list[int]):
+        """
+        Oblicza pozycje nowych wierzchołków w output_graph stosując transformację afiniczną.
+        """
+
+        # 1. Odtworzenie mapowania (L -> G)
+        L_nodes_sorted = sorted(self.L.nodes())
+        if len(mapping) != len(L_nodes_sorted):
+            print("[ERROR] Długość mapowania nie zgadza się z liczbą węzłów w L.")
+            return
+        mapping_dict = {Lnode: Gnode for Lnode, Gnode in zip(L_nodes_sorted, mapping)}
+
+        # 2. Zbieranie punktów do obliczenia transformacji (Wspólne L i Input)
+        src_pts = []
+        dst_pts = []
+
+        for l_node in L_nodes_sorted:
+            g_node = mapping_dict[l_node]
+            # Bierzemy pod uwagę tylko węzły, które mają pozycje w obu grafach
+            if l_node in self.L.pos and g_node in input_graph.pos:
+                src_pts.append(self.L.pos[l_node])
+                dst_pts.append(input_graph.pos[g_node])
+
+        if not src_pts:
+            return  # Brak punktów odniesienia
+
+        src_matrix = np.array(src_pts)
+        dst_matrix = np.array(dst_pts)
+
+        # 3. Obliczenie macierzy transformacji afinicznej M
+        # Równanie: [x_L, y_L, 1] * M = [x_G, y_G]
+        ones = np.ones((len(src_matrix), 1))
+        A = np.hstack([src_matrix, ones])
+
+        if len(src_pts) >= 3:
+            # Metoda najmniejszych kwadratów dla pełnej transformacji (skala, obrót, przesunięcie)
+            M, res, rank, s = np.linalg.lstsq(A, dst_matrix, rcond=None)
+        else:
+            # Fallback dla < 3 punktów: tylko przesunięcie (translacja) na podstawie centroidów
+            centroid_src = np.mean(src_matrix, axis=0)
+            centroid_dst = np.mean(dst_matrix, axis=0)
+            diff = centroid_dst - centroid_src
+            M = np.array([
+                [1.0, 0.0],
+                [0.0, 1.0],
+                [diff[0], diff[1]]
+            ])
+
+        # 4. Identyfikacja faktycznie nowych wierzchołków w Output
+
+        # a) Ustalmy, co zostało usunięte z Input
+        L_nodes = set(self.L.nodes())
+        R_nodes = set(self.R.nodes())
+        to_remove_L = L_nodes - R_nodes
+        to_remove_G = {mapping_dict[ln] for ln in to_remove_L if ln in mapping_dict}
+
+        # b) Ustalmy, co z Input przetrwało (tło + zachowana część produkcji)
+        input_nodes = set(input_graph.nodes())
+        preserved_input_nodes = input_nodes - to_remove_G
+
+        # c) Wszystko w Output, co nie jest "przetrwałe", musi być nowe
+        output_nodes = set(output_graph.nodes())
+        newly_added_ids = output_nodes - preserved_input_nodes
+
+        # Sortujemy po ID, bo apply() przydziela ID rosnąco
+        output_new_ids_sorted = sorted(list(newly_added_ids))
+
+        # d) Nowe węzły z definicji produkcji (R - L) - sortujemy, bo apply() dodaje je w tej kolejności
+        new_R_nodes_sorted = sorted(list(R_nodes - L_nodes))
+
+        # Weryfikacja
+        if len(new_R_nodes_sorted) != len(output_new_ids_sorted):
+            print(
+                f"[WARN] Liczba nowych wierzchołków w R ({len(new_R_nodes_sorted)}) nie zgadza się z wykrytymi w Output ({len(output_new_ids_sorted)}).")
+            return
+
+        # 5. Aplikacja transformacji
+        for r_node, out_id in zip(new_R_nodes_sorted, output_new_ids_sorted):
+            if r_node in self.R.pos:
+                rx, ry = self.R.pos[r_node]
+
+                # Przekształcenie: [rx, ry, 1] * M
+                vec = np.array([rx, ry, 1.0])
+                new_pos = vec @ M
+
+                # Aktualizacja pozycji w grafie wynikowym
+                output_graph.pos[out_id] = tuple(new_pos)
+
+        # output_graph.draw()
+        # plt.show()
--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 
 import src as dp
 # dp.Graph.from_csv("graphs/production_right.csv").draw()
-dp.Graph.from_obj("graphs/graph.obj").draw()
-plt.show()
+# dp.Graph.from_obj("graphs/graph.obj").draw()
+# plt.show()
 
 G = dp.Graph.from_csv("graphs/initial2_graph.csv")
 L = dp.Graph.from_csv("graphs/production_left.csv")
@@ -11,16 +11,18 @@ R = dp.Graph.from_csv("graphs/production_right.csv", pos_like=L.pos)
 # #%%
 # G.draw()
 # #%%
-# def load_mappings(filename):
-#     with open(filename, 'r') as f:
-#         mapping_line = f.readline().strip()
-#         return [int(x.strip()) for x in mapping_line.split(',') if x.strip()]
-# #%%
-# mapping = load_mappings("graphs/mapping.csv")
-# #%%
-# production = dp.Production(L, R)
-# result_graph = production.apply(G, mapping)
-# #%%
+def load_mappings(filename):
+    with open(filename, 'r') as f:
+        mapping_line = f.readline().strip()
+        return [int(x.strip()) for x in mapping_line.split(',') if x.strip()]
+
+mapping = load_mappings("graphs/mapping.csv")
+
+production = dp.Production(L, R)
+result_graph = production.apply(G, mapping, transform_positions=True)
+
 # production.draw()
-# #%%
+# plt.show()
+#
 # result_graph.draw()
+# plt.show()
